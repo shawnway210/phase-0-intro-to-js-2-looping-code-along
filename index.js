2db729bc3267d70ee1920b884e10c183f2b95d0c
shawnconway <shawnway210@gmail.com>
2023-08-10 20:05:15 -0400

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = [ "Porter", "Ari", "Sandra" ];
+function writeCards(names, event){
+    const newArray= []
+    for (let i = 0; i < names.length; i++){
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArray;
+}
+writeCards(names);
+
+
+const numbers = ["10","9","8", "7", "6", "5", "4", "3","2", "1","0"]
+function countDown(){
+    let countDown=0;
+    while(countDown< 11){
+        console.log(countDown++);
+    }
+}
